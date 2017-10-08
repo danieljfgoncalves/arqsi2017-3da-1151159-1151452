5031e260a6ef3b2cac31e87c852ad2229d743f38
ivoferroo <1151159@isep.ipp.pt>
2017-10-08 23:18:03 +0100

    Adds MedicalReceiptDTO and PrescriptionDTO

---
+++ ElectronicPrescription/Models/PrescriptionDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ElectronicPrescription.Models
+{
+    public class PrescriptionDTO
+    {
+        public int Id { get; set; }
+
+        public string ExpirationDate { get; set; }
+
+        public string MedicalReceiptCreationDate { get; set; }
+    }
+}
