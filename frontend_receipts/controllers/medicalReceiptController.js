47c3c040791cdcc402274f9cec88d6f100e16d50
ivoferroo <1151159@isep.ipp.pt>
2017-11-04 00:01:29 +0000

    refactors model on receipts frontend

--- frontend_receipts/controllers/medicalReceiptController.js
+++ frontend_receipts/controllers/medicalReceiptController.js
@@ -2,7 +2,7 @@
 
 var MedicalReceipt = require('../models/medicalReceipt');
 
-// GET /app/medicalReceipts
+// GET /api/medicalReceipts
 exports.get_medical_receipts_list = function(req, res) {
     MedicalReceipt.find(function(err, medicalReceipts) {
         if (err) {
@@ -12,7 +12,7 @@ exports.get_medical_receipts_list = function(req, res) {
     });
 };
 
-// GET /app/medicalReceipts/<id>
+// GET /api/medicalReceipts/<id>
 exports.get_medical_receipt = function(req, res) {
     MedicalReceipt.findById(req.params.id, function(err, medicalReceipt) {
         if (err) {
@@ -22,10 +22,16 @@ exports.get_medical_receipt = function(req, res) {
     });
 };
 
-// POST /app/medicalReceipts
+// POST /api/medicalReceipts
 exports.post_medical_receipt = function(req, res) {
     var medicalReceipt = new MedicalReceipt();
+
     medicalReceipt.creationDate = req.body.creationDate;
+
+    var prescriptionsLength = req.body.prescriptions.length;
+    for (var i = 0; i < prescriptionsLength; i++) {
+        medicalReceipt.prescriptions.push(req.body.prescriptions[i]);
+    }
 
     // save the medical receipt and check for errors
     medicalReceipt.save(function(err) {
@@ -36,13 +42,19 @@ exports.post_medical_receipt = function(req, res) {
     });
 };
 
-// PUT /app/medicalReceipts/<id>
+// PUT /api/medicalReceipts/<id>
 exports.put_medical_receipt = function(req, res) {
     MedicalReceipt.findById(req.params.id, function(err, medicalReceipt) {
         if (err) {
             res.send(err);
         }
+
         medicalReceipt.creationDate = req.body.creationDate;
+
+        var prescriptionsLength = req.body.prescriptions.length;
+        for (var i = 0; i < prescriptionsLength; i++) {
+            medicalReceipt.prescriptions.push(req.body.prescriptions[i]);
+        }
 
         // save the medical receipt and check for errors
         medicalReceipt.save(function(err) {
@@ -54,7 +66,7 @@ exports.put_medical_receipt = function(req, res) {
     });
 };
 
-// DELETE /app/medicalReceipts/<id>
+// DELETE /api/medicalReceipts/<id>
 exports.delete_medical_receipt = function(req, res) {
     MedicalReceipt.remove({
         _id: req.params.id
