bb06393209babbdbfd0a8181b2db1ee1f5f72aa7
Daniel Gonçalves <1151452@isep.ipp.pt>
2017-12-05 12:16:21 +0000

    inits frontend electronic prescriptions

---
+++ frontend-electronic-prescriptions/e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('frontend-electronic-prescriptions App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
